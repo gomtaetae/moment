e6771643deb432f60ec5bc1b62b87fa37531179c
unknown <rlaxodls108@hanmail.net>
2021-08-24 13:31:14 +0900

    clock and name

---
+++ gretting.js
@@ -0,0 +1,41 @@
+const form = document.querySelector(".js-form"),
+      input = form.querySelector("input"),
+      gretting = document.querySelector(".js-gretting");
+
+const USER_LS = "currentUser",
+    SHOWING_CN = "showing";
+
+function saveName(name){
+    localStorage.setItem(USER_LS, name);
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGretting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName(){
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+}
+
+function paintGretting(text){
+    form.classList.remove(SHOWING_CN);
+    gretting.classList.add(SHOWING_CN);
+    gretting.innerText = `Hello ${text}`;
+}
+
+function loadName(){
+    const currentUser = localStorage.getItem(USER_LS);
+    if(currentUser === null) {
+        askForName();
+    } else {
+        paintGretting(currentUser);
+    }
+}
+function init(){
+    loadName();
+}
+init();
