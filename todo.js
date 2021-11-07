02a4a88ec83657a103be0a5a9e12dc9eeb528394
unknown <rlaxodls108@hanmail.net>
2021-11-07 18:22:15 +0900

    todo 삭제기능 추가

--- todo.js
+++ todo.js
@@ -4,7 +4,31 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = 'toDos';
 
-const toDos = [];
+//데이터를 삭제후 재할당이 필요하므로 let을 사용했다.
+let toDos = [];
+
+/*
+//filterFn는 toDo의 id가 1인 li를 필터링 하는 테스트용 함수
+function filterFn(toDo){
+    return toDo.id === 1;
+}
+*/
+
+function deleteToDo(event){
+    //console.log(event.target.parentNode); //log를 이용해 삭제되어야할 정보를 찾는다. li 전체를 지워야함
+    //console.dir(event.target);  //dir, target을 사용해서 li의 parentNode를 찾았다.
+    //parentNode를 log로 출력하고 x버튼을 누르면 li의 정보가 뜨는것을 확인.
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);   //여기까지 실행하면 버튼을 누르면 삭제되지만 정보는 남아있어 새로고침하면 다시 생김.
+    const cleanToDos = toDos.filter(function(toDo){
+        //console.log(toDo.id, li.id); //확인해보면 toDo는 숫자 li는 string이다. 그러므로 li를 int로 바꿔준다.
+        return toDo.id !== parseInt(li.id);
+    });
+    toDos = cleanToDos; //toDos를 삭제된후인 cleanToDos로 바꿔준다.
+    saveToDos();    //삭제되고 다시 바뀐 toDos를 저장해준다.
+    //console.log(cleanToDos);
+}
 
 function saveToDos(){
     //자바스크립트는 localStorage에 있는 모든 데이터를 string으로 저장하려고 한다.
@@ -18,6 +42,7 @@ function paintToDo(text){
     const span = document.createElement("span");
     const newId = toDos.length + 1;
     delBtn.innerText = "❌";
+    delBtn.addEventListener("click", deleteToDo);
     span.innerText = text;
     li.appendChild(span);
     li.appendChild(delBtn);
